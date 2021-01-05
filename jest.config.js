d9d10bee40fd634f151b69bb0df0b62bd11fd1fd
yaquawa <yaquawa@gmail.com>
2021-01-05 16:45:03 +0900

    update dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ export default {
   testMatch: ['<rootDir>/packages/*/tests/**/*.spec.ts'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.vue$': 'vue-jest',
   },
   moduleNameMapper: {
     '^@muku-ui/([^/]*?)$': '<rootDir>/packages/$1/src',
-    '^@muku-ui/((?:.*?/)+.*)': '<rootDir>/packages/$1'
-  }
+    '^@muku-ui/((?:.*?/)+.*)': '<rootDir>/packages/$1',
+  },
 };
